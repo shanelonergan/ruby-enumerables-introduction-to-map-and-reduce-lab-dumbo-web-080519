a5a03cc5a7cddbda70c635981fcb5ec0658f0b12
Shane Lonergan <sptlonergan@gmail.com>
2019-07-26 14:01:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -71,13 +71,17 @@ def reduce_to_all_true(source_array)
 end
 
 def reduce_to_any_true(source_array)
-  i = 0
-  while i < source_array.length do
-
-
-    if source_array[i] != true
-      i += 1
-      return false
-    end
-  end
+  source_array.any?
 end
+
+#def reduce_to_any_true(source_array)
+#  i = 0
+#  while i < source_array.length do
+#
+#
+#    if source_array[i] != true
+#      i += 1
+#      return false
+#    end
+#  end
+#end
