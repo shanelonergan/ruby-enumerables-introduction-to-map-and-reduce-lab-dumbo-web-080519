6bf21ba660e263766600a08616e6d742679dad11
Shane Lonergan <sptlonergan@gmail.com>
2019-07-26 13:56:07 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -67,18 +67,7 @@ def reduce_to_total(source_array, starting_point = 0)
 end
 
 def reduce_to_all_true(source_array)
-  i = 0
-  while i < source_array.length do
-    if source_array[i] != true
-      p false
-      i += 1
-      return
-    elsif source_array[i] = true
-      i+=1
-    end
-  p true
-
-  end
+  source_array.all?
 end
 
 def reduce_to_any_true(source_array)
